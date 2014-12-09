8c968108580b113c8eb5af755013988716a999aa
Mark Camilleri <mark.camilleri@preeostudios.com>
2014-12-09 20:30:12 +0100

    Basic message list
    Typing in text adds messages to chat history

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -2,11 +2,26 @@ var ircApp = angular.module('ircApp', []);
 
 ircApp.controller('ChatRoomCtrl', function ($scope) {
   $scope.users = [
-    {'name': 'markdelf', 'level': 'op' },
-    {'name': 'gerrysaid', 'level': 'voice' },
-    {'name': 'ra', 'level': '' },
-    {'name': 'seth', 'level': '' },
-    {'name': 'zeus', 'level': '' },
-    {'name': 'osiris', 'level': '' }
+    {'username': 'markdelf', 'level': 'op' },
+    {'username': 'gerrysaid', 'level': 'voice' },
+    {'username': 'ra', 'level': '' },
+    {'username': 'seth', 'level': '' },
+    {'username': 'zeus', 'level': '' },
+    {'username': 'osiris', 'level': '' }
   ];
+
+  $scope.chatHistory = [
+    {'username': 'markdelf', 'created': '2014-12-09 20:23', 'message': 'Testing 123'},
+    {'username': 'gerrysaid', 'created': '2014-12-09 20:24', 'message': 'Received ok'},
+  ];
+
+  $scope.onSendMessage = function() {
+    var newText = $("#chatMessage").val();
+    $("#chatMessage").val('');
+    $scope.chatHistory.push({'username': 'markdelf', 'created': '2014-12-09 20:23', 'message': newText});
+  }
+});
+
+$(function(){
+    $("#chatMessage").focus();
 });
