1e0aa12784e5697631c4eaf035fa79f2f79f5061
Mark Camilleri <mark.camilleri@preeostudios.com>
2014-12-10 23:06:50 +0100

    Tabs and windows
    Open and close window functionality. Chat history moved to window
    specific.

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -3,26 +3,33 @@ var ircApp = angular.module('ircApp', []);
 ircApp.controller('ChatRoomCtrl', function ($scope) {
   
   $scope.username = "markdelf";
-  $scope.chatRoom = "#malta";
 
-  $scope.users = [
-    {'username': 'markdelf', 'level': 'op' },
-    {'username': 'gerrysaid', 'level': 'voice' },
-    {'username': 'ra', 'level': '' },
-    {'username': 'seth', 'level': '' },
-    {'username': 'zeus', 'level': '' },
-    {'username': 'osiris', 'level': '' }
+  $scope.windows = [
+        {
+            name: "malta", type: "chatroom",
+            chatHistory: [
+                    {'username': 'markdelf', 'created': '2014-12-09 20:23', 'message': 'Testing 123'},
+                    {'username': 'gerrysaid', 'created': '2014-12-09 20:24', 'message': 'Received ok'},
+            ], 
+            users:  [
+                {'username': 'markdelf', 'level': 'op' },
+                {'username': 'gerrysaid', 'level': 'voice' },
+                {'username': 'ra', 'level': '' },
+                {'username': 'seth', 'level': '' },
+                {'username': 'zeus', 'level': '' },
+                {'username': 'osiris', 'level': '' }
+            ]
+        },
+        {name: "osiris", type: "conversation", chatHistory: [{'username': 'osiris', 'created': '2014-12-09 20:23', 'message': 'Hey'}]},
+        {name: "school", type: "chatroom", chatHistory: [], users:  [{'username': 'markdelf', 'level': 'op' }]}
   ];
 
-  $scope.chatHistory = [
-    {'username': 'markdelf', 'created': '2014-12-09 20:23', 'message': 'Testing 123'},
-    {'username': 'gerrysaid', 'created': '2014-12-09 20:24', 'message': 'Received ok'},
-  ];
+  $scope.activeWindow = 0;
 
-  $scope.onSendMessage = function() {
-    var newText = $("#chatMessage").val();
-    $("#chatMessage").val('');
-    $scope.chatHistory.push(
+  $scope.onSendMessage = function(form) {
+    var newText = $("input.chatMessage:visible").val();
+    $("input.chatMessage:visible").val('');
+    $scope.windows[$scope.activeWindow].chatHistory.push(
         {
             'username': $scope.username,
             'created': moment().format('YYYY-MM-DD, HH:mm'),
@@ -30,8 +37,38 @@ ircApp.controller('ChatRoomCtrl', function ($scope) {
         }
     );
   }
+
+  $scope.selectWindow = function(index) {
+    $scope.activeWindow  = index;
+  }
+
+  $scope.closeWindow = function(index) {
+    $scope.windows.splice(index, 1);
+    if ($scope.activeWindow > index) {
+        $scope.activeWindow--;
+    }
+    if ($scope.activeWindow >= $scope.windows.length) {
+        if ($scope.windows.length >= 1) {
+            $scope.activeWindow = $scope.windows.length - 1;
+        } else {
+            $scope.activeWindow = 0;
+        }
+    }
+    console.log($scope.activeWindow);
+  }
+
+  $scope.openConversation = function(username) {
+    for(var w in $scope.windows) {
+        if ($scope.windows[w].type == "conversation" && $scope.windows[w].name == username) {
+            $scope.activeWindow  = w;
+            return;
+        }
+    }
+    $scope.windows.push({name: username, type: "conversation", chatHistory: []});
+    $scope.activeWindow = $scope.windows.length - 1;
+  }
 });
 
 $(function(){
-    $("#chatMessage").focus();
+    $("input.chatMessage:visible").focus();
 });
